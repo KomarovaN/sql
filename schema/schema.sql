2ca7891a5ffb047df0c25099c503e73c81c60b40
Nadezhda Komarova <grukhinan@gmail.com>
2023-03-18 14:41:55 +0300

    Rework 1: lombok, artifacts, jupiter

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,19 +1,19 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+CREATE TABLE users
+(
     id       CHAR(36) PRIMARY KEY,
-    login VARCHAR(255)      NOT NULL,
-    password VARCHAR(255)   NOT NULL,
-    status   VARCHAR(255)   NOT NULL DEFAULT 'active',
-    UNIQUE (login)
+    login    VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255)        NOT NULL,
+    status   VARCHAR(255)        NOT NULL DEFAULT 'active'
 );
 
 DROP TABLE IF EXISTS cards;
-CREATE TABLE cards(
+CREATE TABLE cards
+(
     id                 CHAR(36) PRIMARY KEY,
-    user_id            CHAR(36)    NOT NULL,
-    number             VARCHAR(19) NOT NULL,
-    balance_in_kopecks INT         NOT NULL,
-    UNIQUE (number),
+    user_id            CHAR(36)           NOT NULL,
+    number             VARCHAR(19) UNIQUE NOT NULL,
+    balance_in_kopecks INT                NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
